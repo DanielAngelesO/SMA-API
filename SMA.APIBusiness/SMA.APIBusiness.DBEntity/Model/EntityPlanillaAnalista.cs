a2891cce42cbd69bfeffc06c60cddeb381eb8830
Hassel Muñoz Lezama <munozhassel@outlook.com>
2022-04-28 13:34:11 -0500

    Creacion de identidades

---
+++ SMA.APIBusiness/SMA.APIBusiness.DBEntity/Model/EntityPlanillaAnalista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMA.APIBusiness.DBEntity.Model
+{
+    internal class EntityPlanillaAnalista
+    {
+        public int Codigo_Analista { get; set; }
+        public string Nombre_Analista { get; set; }
+        public string Tipo { get; set; }
+        public string Grado_Instruccion { get; set; }
+        public string Especialidad { get; set; }
+        public DateTime Fecha_Ingreso { get; set; }
+        public DateTime Fecha_Cese { get; set; }
+    }
+}

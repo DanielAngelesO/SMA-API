86664f73948cf05db3b0395ceb5813ce641d22fe
DANIEL ANGELES <angeles_245@hotmail.com>
2022-05-01 00:48:18 -0500

    corrige sharedComponent solicitud

--- SMA.APIBusiness/SMA.APIBusiness.DBEntity/Model/OrdenServicio/EntityOrdenServcioConsulta.cs
+++ SMA.APIBusiness/SMA.APIBusiness.DBEntity/Model/OrdenServicio/EntityOrdenServcioConsulta.cs
@@ -9,7 +9,12 @@ namespace DBEntity
         public string Codigo_Solicitud { get; set; }
         public string Ruc_Cliente { get; set; }
         public string Razon_Social { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Contacto { get; set; }
+        public string Codigo_Servicio { get; set; }        
         public string Descripcion_Servicio { get; set; }
+        public string TipoServicio { get; set; }
         public decimal Precio_Referencia { get; set; }
         public DateTime Fecha_Inicio { get; set; }
         public DateTime Fecha_Fin { get; set; }
@@ -18,6 +23,9 @@ namespace DBEntity
         public decimal Monto_Viaticos { get; set; }
         public decimal Precio_Final_Servicio { get; set; }
         public string Estado_Solicitud { get; set; }
+        public string Usuario_Registro { get; set; }
+        public DateTime Fecha_Solicitud { get; set; }
+
 
 
     }
